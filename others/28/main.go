5b68d3657a87e20572f846bfc63f082311ff945a
kevin <wushoucheng30510@gmail.com>
2022-07-21 20:21:26 +0800

    Leetcode 28

---
+++ others/28/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	out := strStr("Hello", "ll")
+	fmt.Println(out)
+}
+
+func strStr(haystack string, needle string) int {
+	for index, wordHayStack := range haystack {
+		if len(haystack)-index < len(needle) {
+			return -1
+		}
+		if wordHayStack == rune(needle[0]) {
+			same := compareTwoString(string(haystack[index:index+len(needle)]), needle)
+			if same {
+				return index
+			}
+		}
+	}
+	return -1
+}
+
+func compareTwoString(a, b string) bool {
+	if a == b {
+		return true
+	}
+	return false
+}
